797b42ab489848035e3f04fb35691cfeb54c1a4c
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-02-05 07:15:37 +0900

    AB対応

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
 const DashButton = require('dash-button');
-//const DASH_BUTTON_MAC_ADDRESS = 'a4:d1:8c:e5:bb:16'; // 小文字で。
-const DASH_BUTTON_MAC_ADDRESS = 'f0:27:2d:8d:ad:e1'; // 小文字で。
-//const DASH_BUTTON_MAC_ADDRESS = '68:54:fd:6e:74:32'; // 小文字で。
- 
-let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
+const request = require('request');
+
+const FINISH_DASH_MAC_ADDRESS = 'f0:27:2d:8d:ad:e1';
+const RENOA_DASH_MAC_ADDRESS = '88:71:e5:d9:8b:9a';
+const FABREEZ_DASH_MAC_ADDRESS = '34:d2:70:d6:f5:d6';
+const WILKIN_DASH_MAC_ADDRESS = '68:54:fd:3b:d5:09';
+
+let buttonA1 = new DashButton(FINISH_DASH_MAC_ADDRESS);
+let buttonA2 = new DashButton(RENOA_DASH_MAC_ADDRESS);
+let buttonB1 = new DashButton(FABREEZ_DASH_MAC_ADDRESS);
+let buttonB2 = new DashButton(WILKIN_DASH_MAC_ADDRESS);
 
 console.log('listen...');
 
-let subscription = button.addListener( () => {
-  console.log('Clicked..'+ new Date());
-  getApi();
+buttonA1.addListener( () => {
+  console.log('Clicked..A1'+ new Date());
+  getApiA();
 });
 
-var request = require('request');
-var options = {
+buttonA2.addListener( () => {
+    console.log('Clicked..A2'+ new Date());
+    getApiA();
+});
+
+buttonB1.addListener( () => {
+    console.log('Clicked..B1'+ new Date());
+    getApiB();
+});
+
+buttonB2.addListener( () => {
+      console.log('Clicked..B2'+ new Date());
+      getApiB();
+});
+
+var optionsA = {
   uri: "http://state-api.au-syd.mybluemix.net/state/clickableA",
   headers: {
     "Content-type": "application/json",
   },
 };
-console.log(options);
 
-function getApi() {
+var optionsB = {
+    uri: "http://state-api.au-syd.mybluemix.net/state/clickableB",
+    headers: {
+          "Content-type": "application/json",
+        },
+};
+
+function getApiA() {
   request.get(options, function(error, response, body){
-  if (!error && response.statusCode == 200) {
+    if (body.match(/true/)) {
       console.log(body);
     } else {
       console.log('error: '+ response.statusCode);
     }
 });
-}
+
+function getApiB() {
+  request.get(options, function(error, response, body){
+    if (body.match(/true/)) {
+          console.log(body);
+        } else {
+          console.log('error: '+ response.statusCode);
+        }
+  });
+}}
